c7dd6c10bc0f3d8f783455c5ad37472e4e1935cc
Akira Wong <awongh@gmail.com>
2021-01-15 00:15:31 +0800

    Initial commit

---
+++ webpack_conf/webpack.dev.js
@@ -0,0 +1,47 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = merge(common, {
+  entry: {
+    main: ['webpack-hot-middleware/client', './src/index.js'],
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx)$/, // regex to see which files to run babel on
+        exclude: /node_modules/,
+        use: {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react'],
+            plugins: [
+              require.resolve('react-refresh/babel'),
+            ].filter(Boolean),
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin({
+        overlay: {
+          sockIntegration: 'whm',
+        }
+    }),
+    new HtmlWebpackPlugin({
+      // name this file main, so that it does not get automatically requested as a static file
+      filename:'./main.html',
+      template: path.resolve(__dirname, '..', 'src', 'index.html'),
+    }),
+
+  ].filter(Boolean),
+});
